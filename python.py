01b02ef56dab20737c0e363689f8da78052d5dd5
Shahbaz-Bashir <shahbaz.bashir.nbt@gmail.com>
2024-11-18 17:16:48 +0500

    Variable

---
+++ python.py
@@ -0,0 +1,52 @@
+
+
+#name = "shahbaz"
+#age =   30
+#email = "shahbaz.emmail@@gmail.com"
+#print(f"My name is {name} and age is {age} and my email is {email}")
+
+#print('shahbaz')
+#print("Shahbaz")
+#print('''Some" "said" this is good''')  
+#print("Shahbaz", "Bashir","Waqas")
+#print("Shahbaz", "Bashir","Waqas", sep="-")
+#print("Shahbaz \n Bashir \t Waqas")
+#print("Shahbaz\"Ali\" \'Azeem\'")
+
+#Variable
+#name = "Shahbaz"
+#print(name)
+#name = "Shahbaz"
+#print(type(name))
+#name = 25
+#print(type(name))
+#name = 8.8
+#print(type(name))
+#name = True
+#print(type(name))
+
+#number = 10
+#print(number)
+#2number = 15
+#print(2number)
+#_number = 12
+#print(_number)
+
+#name = "Shahbaz"
+#print(name)
+
+#email = " Shahbaz.email.com"
+#print(email)
+
+name = "Shahbaz Bashir "
+print(name[0] , end= "")
+print(name[1] , end= "")
+print(name[2] , end= "")
+print(name[3] , end= "")
+print(name[4] , end= "")
+print(name[5] , end= "")
+print(name[6])
+
+print(name[0:7] )
+
+
